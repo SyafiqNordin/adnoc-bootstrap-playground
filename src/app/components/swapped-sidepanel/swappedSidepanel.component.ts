2c322d2c61731dbed117e7f8a699adfdba8a7139
aniszulaikha <anis@seetrustudio.com>
2024-11-08 15:26:58 +0800

    swap panel animation
    styling wip

--- src/app/components/swapped-sidepanel/swappedSidepanel.component.ts
+++ src/app/components/swapped-sidepanel/swappedSidepanel.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'swapped-sidepanel',
     templateUrl: './swappedSidepanel.component.html',
     styleUrl: './swappedSidepanel.component.scss',
     standalone: true,
-    imports: [RouterLink],
+    imports: [RouterModule, CommonModule],  // Add CommonModule here
 })
 export class swappedSidepanelComponent {
+    isOneFront = true;
+    isTwoFront = false;
 
+    toggleOne() {
+        if (this.isOneFront) {
+            console.log('has front class');
+        } else {
+            this.isOneFront = true;
+            this.isTwoFront = false;
+        }
+    }
+
+    toggleTwo() {
+        if (this.isTwoFront) {
+            console.log('has front class');
+        } else {
+            this.isTwoFront = true;
+            this.isOneFront = false;
+        }
+    }
 }
