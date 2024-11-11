add007c6e58a11ec9dc4398af1cec236097614f5
Muhammad Syafiq bin Nordin <syafiq.nordin@seetrustudio.com>
2024-11-11 15:03:31 +0800

    Added Field Network Component

---
+++ src/app/components/field-network/fieldNetwork.component.ts
@@ -0,0 +1,38 @@
+import {
+  Component,
+  ViewEncapsulation,
+  CUSTOM_ELEMENTS_SCHEMA,
+} from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { setAssetPath } from '@esri/calcite-components/dist/components';
+setAssetPath('https://js.arcgis.com/calcite-components/2.13.2/assets');
+
+import '@arcgis/map-components/dist/components/arcgis-map';
+import '@arcgis/map-components/dist/components/arcgis-zoom';
+import '@arcgis/map-components/dist/components/arcgis-search';
+import '@arcgis/map-components/dist/components/arcgis-legend';
+import '@arcgis/map-components/dist/components/arcgis-bookmarks';
+import '@arcgis/map-components/dist/components/arcgis-expand';
+import '@arcgis/map-components/dist/components/arcgis-basemap-toggle';
+import '@arcgis/map-components/dist/components/arcgis-basemap-gallery';
+
+@Component({
+  selector: 'field-network',
+  templateUrl: './fieldNetwork.component.html',
+  styleUrls: ['./fieldNetwork.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
+  imports: [RouterLink],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class FieldNetworkComponent {
+  /**
+   * Constructor
+   */
+  constructor() {}
+
+  arcgisViewReadyChange(event: any) {
+    console.log('MapView ready', event);
+  }
+}
---
+++ src/app/components/field-network/fieldNetwork.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { FieldNetworkComponent } from '../field-network/fieldNetwork.component';
+
+export default [
+    {
+        path: '',
+        component: FieldNetworkComponent,
+    },
+] as Routes;
