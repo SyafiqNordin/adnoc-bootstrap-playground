cd4e7085116fed841a15a0a451f0f0b462bde9f2
Muhammad Syafiq bin Nordin <syafiq.nordin@seetrustudio.com>
2024-11-12 23:37:51 +0800

    Added layout, card placeholders in dashboard page and side menu component

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,16 +1,22 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, EventEmitter, Output } from '@angular/core';
 import { RouterLink } from '@angular/router';
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html',
-    styleUrls: ['./header.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    standalone: true,
-    imports: [RouterLink],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  standalone: true,
+  imports: [RouterLink],
 })
 export class HeaderComponent {
-    /**
-     * Constructor
-     */
-    constructor() {}
+  @Output() isSideMenuOpen = new EventEmitter<boolean>();
+
+  /**
+   * Constructor
+   */
+  constructor() {}
+
+  togglePanel() {
+    this.isSideMenuOpen.emit(true);
+  }
 }
--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,16 +1,20 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { SideMenuComponent } from '../../components/side-menu/sideMenu.component';
 @Component({
     selector: 'app-main-layout',
     templateUrl: './main-layout.component.html',
     styleUrls: ['./main-layout.component.scss'],
     encapsulation: ViewEncapsulation.None,
     standalone: true,
-    imports: [RouterLink, RouterModule, HeaderComponent],
+    imports: [RouterLink, RouterModule, HeaderComponent, SideMenuComponent, CommonModule],
 })
 export class MainLayoutComponent {
+    isSideMenuOpen: boolean = false;
+
     /**
      * Constructor
      */
