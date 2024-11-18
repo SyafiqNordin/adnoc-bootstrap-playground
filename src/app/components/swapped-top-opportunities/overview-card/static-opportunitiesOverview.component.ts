e69011d1d4569809219a60a1c2e23da748a8f08b
aniszulaikha <anis@seetrustudio.com>
2024-11-18 14:39:39 +0800

    fix styling

--- src/app/components/swapped-top-opportunities/overview-card/static-opportunitiesOverview.component.ts
+++ src/app/components/swapped-top-opportunities/overview-card/static-opportunitiesOverview.component.ts
@@ -9,5 +9,5 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
 })
 export class StaticOpportunitiesOverviewComponent {
-  @Input() opportunities: Array<{ well: string, expectedGain: number, percentageChange: string }> = [];
+  @Input() opportunities: Array<{ well: string, expectedGain: number, percentageChange: string, beforeOptimize: number }> = [];
 }
