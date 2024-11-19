c2db5a6fee63d3decc9d6bde129a13c8fd1a3b84
aniszulaikha <anis@seetrustudio.com>
2024-11-19 16:20:09 +0800

    fix styling & update icons

--- src/app/components/swapped-top-underperform/listing-card/static-underperformListing.component.ts
+++ src/app/components/swapped-top-underperform/listing-card/static-underperformListing.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { KENDO_BUTTON } from "@progress/kendo-angular-buttons";
 
 @Component({
   selector: 'static-underperform-listing',
   templateUrl: './static-underperformListing.component.html',
   styleUrls: ['./static-underperformListing.component.scss'],
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    KENDO_BUTTON,
+  ],
 })
 export class StaticUnderperformListingComponent {
   @Input() underperform: Array<{ well: string, deferment: number, percentageChange: string }> = [];
